50c12b845bc9c7a46e34e46a9ac18f3e425d2594
unknown <tony.cherednichenko95@yandex.ru>
2025-03-29 14:56:00 +0300

    Updates

---
+++ assets/Button.stories-Cf_k7Um9.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";const e=()=>o.jsx("button",{children:"click me"});e.__docgenInfo={description:"",methods:[],displayName:"Button"};const n={title:"Button",component:e,parameters:{layout:"centered"},argTypes:{type:"button"}},t={args:{type:"button"}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: 'button'
+  }
+}`,...t.parameters?.docs?.source}}};const s=["DefaultButton"];export{t as DefaultButton,s as __namedExportsOrder,n as default};
--- assets/Button.stories-DdcaLsMh.js
+++
@@ -1,3 +0,0 @@
-import{j as o}from"./jsx-runtime-D_zvdyIk.js";const e=()=>o.jsx("button",{children:"click me"});e.__docgenInfo={description:"",methods:[],displayName:"Button"};const s={title:"Button",component:e,parameters:{layout:"centered"},argTypes:{}},t={args:{}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...t.parameters?.docs?.source}}};const a=["DefaultButton"];export{t as DefaultButton,a as __namedExportsOrder,s as default};
